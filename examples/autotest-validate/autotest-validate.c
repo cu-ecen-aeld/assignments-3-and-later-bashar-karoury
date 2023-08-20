e5143d84844e18f49c28ef3edbda0c660a476d1d
bashar-karoury <basharalkaroury@hotmail.com>
2023-08-20 13:21:22 +0300

    update username at examples/autotest-validate/autotest-validate.c

--- examples/autotest-validate/autotest-validate.c
+++ examples/autotest-validate/autotest-validate.c
@@ -23,9 +23,9 @@ bool this_function_returns_false()
 
 /**
  * @return a string which contains the username you use for
- * git submissions.  This string should match the string in conf/username.txt
+ * git submissions.y this string should match the string in conf/username.txt
  */
 const char *my_username()
 {
-    return "todo-please-enter-your-username-here-in-my_username";
+    return "bashar-karoury";
 }
